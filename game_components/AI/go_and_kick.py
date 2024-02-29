0260d11d2b4c8e84358cfaa4fc9d94fa98305284
payur <payurchyshyn@gmail.com>
2024-02-29 22:09:20 +0200

    AI improve

--- game_components/AI/go_and_kick.py
+++ game_components/AI/go_and_kick.py
@@ -1,6 +1,6 @@
 from game_components.AI.base import GoToPerson, TaskState
 from game_components.character.user_character import Character
-from game_components.constants import KICK_VELOCITY
+from game_components.constants import KICK_VELOCITY, FIST_HIT_DAMAGE
 from game_components.game import Game
 from game_components.sounds import play_kick_sound
 
@@ -14,6 +14,7 @@ class GoAndKick(GoToPerson):
             kick_direction = -1 if self.target.position[0] < character.position[0] else 1
             self.target.horizontal_velocity = kick_direction * KICK_VELOCITY
             self.target.vertical_velocity -= KICK_VELOCITY
+            self.target.damage(damage=FIST_HIT_DAMAGE, reason=f'копняк від @{character.name}')
             play_kick_sound()
 
         return res
--- game_components/AI/go_and_kiss.py
+++ game_components/AI/go_and_kiss.py
@@ -3,6 +3,7 @@ from game_components.character.user_character import Character
 from game_components.game import Game
 from game_components.events.flying_heart import FlyingHeart
 from game_components.sounds import play_kiss_sound
+from game_components.constants import KISS_HEAL_VALUE
 
 
 class GoAndKiss(GoToPerson):
@@ -13,5 +14,7 @@ class GoAndKiss(GoToPerson):
         if res == self.STATUS.Done:
             game_obj.add_event(FlyingHeart(self.target.position))
             game_obj.add_event(FlyingHeart(character.position))
+            character.heal(KISS_HEAL_VALUE)
+            self.target.heal(KISS_HEAL_VALUE)
             play_kiss_sound()
         return res
