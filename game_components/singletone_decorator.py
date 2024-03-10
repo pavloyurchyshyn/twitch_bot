540e3a086b6939cbaf256333e08ba537b866dd6b
payur <payurchyshyn@gmail.com>
2024-03-10 19:39:44 +0200

    moved memory

--- game_components/singletone_decorator.py
+++ game_components/singletone_decorator.py
@@ -1,6 +1,7 @@
-def single_tone_decorator(cls):
-    memory = {}
+memory = {}
 
+
+def single_tone_decorator(cls):
     def wrapper(**kwargs):
         if cls.__name__ not in memory:
             memory[cls.__name__] = cls(**kwargs)
